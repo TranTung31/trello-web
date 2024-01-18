ac36cd10a1b6e854ac0c994bc0143136f360b0cc
Your Name <you@example.com>
2024-01-18 15:38:14 +0700

    dark_light_p1

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,25 @@ import Button from '@mui/material/Button'
 import { pink } from '@mui/material/colors'
 import HomeIcon from '@mui/icons-material/Home'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle />
       <div>Hello world!</div>
       <Typography variant='body1' color="text.secondary">ReactJS</Typography>
       <Button variant="text">Text</Button>
