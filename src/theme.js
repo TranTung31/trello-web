707aa4eb2d7f80dced5a60f8094acb08720a334d
Your Name <you@example.com>
2024-01-21 16:56:14 +0700

    App Bar P1

--- src/theme.js
+++ src/theme.js
@@ -6,8 +6,8 @@ import {
 // Create a theme instance.
 const theme = extendTheme({
   trello: {
-    appBarHeight: '48px',
-    boardBarHeight: '58px'
+    appBarHeight: '58px',
+    boardBarHeight: '60px'
   },
   colorSchemes: {
     light: {
