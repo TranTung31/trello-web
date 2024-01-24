5685cc63da68b1bb43764c8789453855f5040f89
Your Name <you@example.com>
2024-01-24 22:28:01 +0700

    App Bar P2

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -47,11 +47,11 @@ function AppBar() {
         <ModeSelect />
         <Tooltip title="Notification">
           <Badge color="secondary" variant='dot' sx={{ cursor: 'pointer' }}>
-            <NotificationsNoneIcon/>
+            <NotificationsNoneIcon sx={{ color: 'primary.main' }}/>
           </Badge>
         </Tooltip>
         <Tooltip title="Help">
-          <HelpOutlineIcon sx={{ cursor: 'pointer' }}/>
+          <HelpOutlineIcon sx={{ cursor: 'pointer', color: 'primary.main' }}/>
         </Tooltip>
         <Profiles />
       </Box>
