947e85369bd7461f57927ee7b7c254e9bef57dba
Your Name <you@example.com>
2024-01-26 14:49:31 +0700

    App Bar P3

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -18,13 +18,14 @@ import Profiles from './Menus/Profiles'
 
 function AppBar() {
   return (
-    <Box sx={{
+    <Box px={2} sx={{
       width: '100%',
       height: (theme) => theme.trello.appBarHeight,
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'space-between',
-      px: 2
+      gap: 2,
+      overflow: 'auto'
     }}>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <AppsIcon sx={{ color: 'primary.main' }}/>
@@ -35,15 +36,16 @@ function AppBar() {
           </Typography>
         </Box>
 
-        <Workspaces />
-        <Recent />
-        <Starred />
-        <Templates />
-
-        <Button variant="outlined">Create</Button>
+        <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 1 }}>
+          <Workspaces />
+          <Recent />
+          <Starred />
+          <Templates />
+          <Button variant="outlined">Create</Button>
+        </Box>
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-        <TextField id="outlined-search" label="Search..." type="search" size='small' />
+        <TextField id="outlined-search" label="Search..." type="search" size='small' sx={{ minWidth: 120 }} />
         <ModeSelect />
         <Tooltip title="Notification">
           <Badge color="secondary" variant='dot' sx={{ cursor: 'pointer' }}>
