3e498b1050dcf7c0d9c0f038c3d7921a505702d7
Your Name <you@example.com>
2024-02-01 15:12:19 +0700

    Board Bar

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,15 +1,138 @@
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import { Tooltip } from '@mui/material'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Chip from '@mui/material/Chip'
 
 function BoardBar() {
+  const MENU_STYLES = {
+    color: 'primary.main',
+    bgcolor: 'white',
+    paddingX: '5px',
+    borderRadius: '4px',
+    '& .MuiSvgIcon-root': {
+      color: 'primary.main'
+    },
+    '&:hover': {
+      bgcolor: 'primary.50'
+    }
+  }
+
   return (
     <Box sx={{
       width: '100%',
       height: (theme) => theme.trello.boardBarHeight,
-      backgroundColor: 'primary.dark',
+      borderTop: '1px solid #00bfa5',
+      backgroundColor: 'white',
+      paddingX: 2,
       display: 'flex',
-      alignItems: 'center'
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      overflow: 'auto'
     }}>
-      Board bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          icon={<DashboardIcon />}
+          label="TranTungDev Fullstack"
+          clickable
+          sx={MENU_STYLES}
+        />
+        <Chip
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+          sx={MENU_STYLES}
+        />
+        <Chip
+          icon={<AddToDriveIcon />}
+          label="Add to Google Drive"
+          clickable
+          sx={MENU_STYLES}
+        />
+        <Chip
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+          sx={MENU_STYLES}
+        />
+        <Chip
+          icon={<FilterListIcon />}
+          label="Filters"
+          clickable
+          sx={MENU_STYLES}
+        />
+      </Box>
+
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button variant="outlined" startIcon={<PersonAddIcon />}>Invite</Button>
+        <AvatarGroup
+          max={7}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16
+            }
+          }}
+        >
+          <Tooltip title='trantungdev'>
+            <Avatar
+              alt="trantungdev"
+              src="https://fullstack.edu.vn/static/media/f8-icon.18cd71cfcfa33566a22b.png"
+            />
+          </Tooltip>
+          <Tooltip title='trantungdev'>
+            <Avatar
+              alt="trantungdev"
+              src="https://fullstack.edu.vn/static/media/f8-icon.18cd71cfcfa33566a22b.png"
+            />
+          </Tooltip>
+          <Tooltip title='trantungdev'>
+            <Avatar
+              alt="trantungdev"
+              src="https://fullstack.edu.vn/static/media/f8-icon.18cd71cfcfa33566a22b.png"
+            />
+          </Tooltip>
+          <Tooltip title='trantungdev'>
+            <Avatar
+              alt="trantungdev"
+              src="https://fullstack.edu.vn/static/media/f8-icon.18cd71cfcfa33566a22b.png"
+            />
+          </Tooltip>
+          <Tooltip title='trantungdev'>
+            <Avatar
+              alt="trantungdev"
+              src="https://fullstack.edu.vn/static/media/f8-icon.18cd71cfcfa33566a22b.png"
+            />
+          </Tooltip>
+          <Tooltip title='trantungdev'>
+            <Avatar
+              alt="trantungdev"
+              src="https://fullstack.edu.vn/static/media/f8-icon.18cd71cfcfa33566a22b.png"
+            />
+          </Tooltip>
+          <Tooltip title='trantungdev'>
+            <Avatar
+              alt="trantungdev"
+              src="https://fullstack.edu.vn/static/media/f8-icon.18cd71cfcfa33566a22b.png"
+            />
+          </Tooltip>
+          <Tooltip title='trantungdev'>
+            <Avatar
+              alt="trantungdev"
+              src="https://fullstack.edu.vn/static/media/f8-icon.18cd71cfcfa33566a22b.png"
+            />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
