26b1b56ef206516376745d432b0fcaa21d59c8b2
Your Name <you@example.com>
2024-03-16 15:50:21 +0700

    feat: axios call api

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,15 +3,27 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    const boardId = '65f54fea1704597778997cc1'
+
+    fetchBoardDetailAPI(boardId).then(board => {
+      setBoard(board)
+    })
+  }, [])
+
   return (
     // 100vh là chiều cao tự thay đổi theo độ dài của trình duyệt
     // disableGutters maxWidth={true} giúp chiều rộng của Container chiếm full màn hình
     <Container disableGutters maxWidth={true} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board}/>
-      <BoardContent board={mockData?.board}/>
+      <BoardBar board={board}/>
+      <BoardContent board={board}/>
     </Container>
   )
 }
